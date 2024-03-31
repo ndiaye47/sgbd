cdb76dbdd97909bf8c8e1af6d8ed7523a69ff2f8
medz1511 <134383184+medz1511@users.noreply.github.com>
2024-03-31 06:25:34 +0000

    création des onglet admin ,Client,Produit et  de la fenetre

--- opencart.py
+++ opencart.py
@@ -31,3 +31,37 @@ connection = mysql.connector.connect(
      database="opencartbd"
  )
 cursor = connection.cursor()
+
+
+# Création de la fenêtre Tkinter
+root = tk.Tk()
+root.title("Monitoring OpenCart")
+
+
+# Création des onglets
+root.title("Monitoring OpenCart")
+
+
+menuPrincipale = Menu(root)
+
+menuPrincipale.add_cascade(label="admin")
+menuPrincipale.add_cascade(label="Client")
+menuPrincipale.add_cascade(label="Produit")
+
+root.config(menu=menuPrincipale)
+# Création des boutons pour afficher différentes options
+user_button = tk.Button(root, text="Nombre d'utilisateurs", command=get_user_count)
+user_button.pack(pady=5)
+
+online_user_button = tk.Button(root, text="Nombre d'utilisateurs connectés", command=get_online_user_count)
+online_user_button.pack(pady=5)
+
+ordered_product_button = tk.Button(root, text="Nombre de produits commandés", command=get_ordered_product_count)
+ordered_product_button.pack(pady=5)
+
+# Étiquette pour afficher le résultat
+result_label = tk.Label(root, text="")
+result_label.pack(pady=10)
+
+# Exécution de la boucle principale Tkinter
+root.mainloop()
