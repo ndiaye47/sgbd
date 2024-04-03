8826a56b558b094caa49afe9578c7094596d3710
medz1511 <134383184+medz1511@users.noreply.github.com>
2024-04-03 13:01:43 +0000

    teste commit

--- opencart.py
+++ opencart.py
@@ -96,5 +96,5 @@ total_products_button.pack(pady=5)
 result_label = tk.Label(root, text="")
 result_label.pack(pady=10)
 
-# Exécution de la boucle principale Tkinter
+# Exécution de la boucle principale Tkinters
 root.mainloop()
